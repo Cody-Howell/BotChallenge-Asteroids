23a0f52d9bc51e1b95df1a604aa807dc8dc429c6
Moosenwolf <codyhowell44@gmail.com>
2026-01-02 13:44:05 -0700

    thanks for helping me debug the configclass ai

--- BC-Asteroids/BC-Asteroids.Shared/Bullet.cs
+++ BC-Asteroids/BC-Asteroids.Shared/Bullet.cs
@@ -6,15 +6,17 @@ namespace BC_Asteroids.Shared;
 public class Bullet : GameObject {
     public bool ShouldBeDestroyed { get => Countdown < 0; }
     public int PlayerId { get; private set; }
-    public int Countdown = ConfigClass.Config["bullet"]["duration"].AsInt();
+    public int Countdown;
     private static double Radius { get => ConfigClass.Config["bullet"]["radius"].AsDouble(); }
 
     public Bullet(Point2D center, Vector2D vector, int id) : base(center, vector, Radius) {
         PlayerId = id;
+        Countdown = ConfigClass.Config["bullet"]["duration"].AsInt();
     }
 
-    public Bullet(Point2D center, Vector2D vector, int id, double radius) : base(center, vector, radius) {
+    public Bullet(Point2D center, Vector2D vector, int id, double radius, int countdown) : base(center, vector, radius) {
         PlayerId = id;
+        Countdown = countdown;
     }
 
 
@@ -31,9 +33,7 @@ public class Bullet : GameObject {
         string[] items = input.Split(' ');
         Point2D center = new(Convert.ToDouble(items[1]), Convert.ToDouble(items[2]));
         Vector2D vec = new(Convert.ToDouble(items[3]), Convert.ToDouble(items[4]));
-        Bullet b = new(center, vec, Convert.ToInt32(items[0]), Convert.ToDouble(items[6])) {
-            Countdown  = Convert.ToInt32(items[5])
-        };
+        Bullet b = new(center, vec, Convert.ToInt32(items[0]), Convert.ToDouble(items[6]), Convert.ToInt32(items[5]));
 
         return b;
     }
