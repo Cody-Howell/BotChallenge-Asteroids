988e6b88a0e5ac58a69e53c80fbdb048f9ccca28
Moosenwolf <codyhowell44@gmail.com>
2026-01-23 19:50:17 -0700

    made some adjustments to the testing system and now I'm done :D

--- BC-Asteroids/BC-Asteroids.Training/FileWriter.cs
+++ BC-Asteroids/BC-Asteroids.Training/FileWriter.cs
@@ -1,11 +1,15 @@
 using HowlDev.AI.Core;
+using HowlDev.AI.Core.Classes;
 
 namespace BC_Asteroids.Training;
 
-public class FileWriter : IFileWriter {
-    public void WriteFile(string path, string value) {
-        Console.WriteLine($"Writing file: {path}");
-        Directory.CreateDirectory("./output");
-        File.WriteAllText("./output/" + path + ".txt", value);
+public class FileWriter : IResultReader {
+    public void ReadResult(GeneticAlgorithmResult result) {
+        Console.WriteLine($"New generation: {result.Generation}");
+        if (result.Generation % 5 == 0) {
+            Directory.CreateDirectory("./output");
+            File.WriteAllText($"./output/Gen-{result.Generation}-Results.txt", string.Join('\n', result.Results));
+            File.WriteAllText($"./output/Gen-{result.Generation}-Best-Network.txt", result.Networks[0].network);
+        }
     }
 }
--- BC-Asteroids/BC-Asteroids.Training/Program.cs
+++ BC-Asteroids/BC-Asteroids.Training/Program.cs
@@ -1,6 +1,7 @@
 ﻿using BC_Asteroids.Shared;
 using BC_Asteroids.Shared.Config;
 using BC_Asteroids.Training;
+using HowlDev.AI.Structures.NeuralNetwork.Options;
 using HowlDev.AI.Training.Genetic;
 using HowlDev.AI.Training.Saving;
 
@@ -9,8 +10,9 @@ ConfigClass.Initialize("./config.json");
 GeneticAlgorithm<AsteroidGame> algo = new(new(new() { CountPerGroup = 5, NumberOfGroups = 100, NumOfGenerations = 5000, NumberOfTicks = 3000 },
     new(23, [5], 3),
     new()) {
-    SavingStrategy = new() { SavingScheme = NetworkSavingScheme.Best }
-},
+    SavingStrategy = NetworkSavingScheme.Best,     
+    FunctionKind = ActivationFunctionKind.Tanh
+}, 
 new FileWriter());
 
-await algo.StartTraining();
+algo.StartTraining();
